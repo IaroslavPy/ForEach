a430cf9c9260d3e9a676bbc4ae78456942a673a6
IaroslavPy <IaroslavPylypchuk@gmail.com>
2023-02-07 16:49:37 +0200

    Add average method

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,7 @@ public class Main {
         int [] array = {1, 3, 4, 10, -19};
         getSum(array);
         System.out.println("Version 1.0");
+        System.out.print("Average: " + getAverage(array));
     }
 
     private static void getSum(int[] array){
@@ -13,6 +14,13 @@ public class Main {
             sum = sum + digit;
         }
         System.out.print("Sum: " + sum);
+    }
+    private static double getAverage(int[] array){
+        int sum = 0;
+        for (int digit: array) {
+            sum = sum + digit;
+        }
+        return 1.0D * sum / array.length;
     }
 
 }
