31a99f643dd87d003fbe13ef2479db5651e63729
IaroslavPy <IaroslavPylypchuk@gmail.com>
2023-02-07 16:45:30 +0200

    Add version

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int [] array = {1, 3, 4, 10, -19};
         getSum(array);
+        System.out.println("Version 1.0");
     }
 
     private static void getSum(int[] array){
